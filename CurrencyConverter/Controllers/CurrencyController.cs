6891fc602713a85bf19f97df334be6e930c2d99e
Jose Montanez <josemontanez1992@gmail.com>
2023-05-02 11:00:03 -0600

    Initial commit

---
+++ CurrencyConverter/Controllers/CurrencyController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CurrencyConverter.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CurrencyConverter.Controllers
+{
+    public class CurrencyController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            CurrencyViewModel model = new CurrencyViewModel();
+            return View(model);
+        }
+
+        public IActionResult Convert(CurrencyViewModel model)
+        {
+            return View();
+        }
+    }
+}
+
---
+++ CurrencyConverter/Models/CurrencyViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace CurrencyConverter.Models
+{
+	public class CurrencyViewModel
+	{
+		public string FromCurrency { get; set; }
+		public string ToCurrency { get; set; }
+		public int Amount { get; set; }
+		public List<string> CurrencyList { get; set; }
+		public int ConvertedResult { get; set; }
+
+		public CurrencyViewModel()
+		{
+			CurrencyList = new List<string>
+			{
+				"USD",
+				"EUR"
+			};
+		}
+	}
+}
+
